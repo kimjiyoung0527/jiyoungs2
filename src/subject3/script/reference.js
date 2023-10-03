82a92099658f8a39a90cbe582ac36598859c2335
kimjiyoung0527 <143327785+kimjiyoung0527@users.noreply.github.com>
2023-10-03 11:22:41 +0900

    2차제출수정

--- src/subject3/script/reference.js
+++ src/subject3/script/reference.js
@@ -1,9 +1,9 @@
 const reference = [
   {
-    author: "",
-    title: "",
-    year: "",
-    sourceTitle: "",
-    link: "",
+    author: 'Good Mockups',
+    title: 'Free Duct Tape Strips Mockup PSD',
+    year: '2023',
+    sourceTitle: '',
+    link: 'https://goodmockups.com/free-duct-tape-strips-mockup-psd/',
   },
 ];
